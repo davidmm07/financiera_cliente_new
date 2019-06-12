136696fe74cbdf8d7e3bac34cdd05067cb825ea4
macaguegi <mcguerrerog@correo.udistrital.edu.co>
2019-06-12 11:35:44 -0500

    Menu con item  Apropiaciones #8
    Se añade item de Apropiaciones al menu principal y se enlazan los modulos y componentes correspondientes

--- src/app/pages/apropiaciones/apropiaciones.module.ts
+++ src/app/pages/apropiaciones/apropiaciones.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApropiacionesRoutingModule, routedComponents } from './apropiaciones-routing.module';
 
-
+// routedComponents of Module Apropiaciones
 @NgModule({
   declarations: [...routedComponents,],
   imports: [
