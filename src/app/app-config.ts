23067d4fa6cc8d63a8cf3bcdb8dae5919c7dd9d2
@Soulfilip <efcanastoj@udistrital.edu.co>
2019-07-05 01:25:04 +0900

    adding DEVELOP environment and fixing Url that di not end with /

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,14 +1,36 @@
 import { environment } from '../environments/environment';
 
 export const Config = {
+  DEVELOP: {
+    NUXEO: {	       
+        PATH: 'https://documental.udistrital.edu.co/nuxeo/',
+    },
+    WSO2_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas',
+    PLAN_CUENTAS_CRUD_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_crud/v1/',
+    PLAN_CUENTAS_MID_SERVICE:'http://localhost:8084/',
+    PLAN_CUENTAS_MONGO_SERVICE:'http://localhost:8082/',
+    CONFIGURACION_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
+    NOTIFICACION_SERVICE: 'wss://autenticacion.udistrital.edu.co/apioas/notificacion_ws/v1/',
+    CONF_MENU_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
+    TOKEN: {
+      AUTORIZATION_URL: 'https://autenticacion.udistrital.edu.co/oauth2/authorize',
+      CLIENTE_ID: 'TJ43YHyy3T29GrlTx9l0j7J7Cl8a',
+      RESPONSE_TYPE: 'id_token token',
+      SCOPE: 'openid email role',
+      REDIRECT_URL: 'http://localhost:9000/',
+      SIGN_OUT_URL: 'https://autenticacion.udistrital.edu.co/oidc/logout',
+      SIGN_OUT_REDIRECT_URL: 'http://localhost:9000',
+      REFRESH_TOKEN: 'https://autenticacion.udistrital.edu.co/oauth2/token',
+    },
+  },
   LOCAL: {
     NUXEO: {	       
         PATH: 'https://documental.udistrital.edu.co/nuxeo/',
     },
     WSO2_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas',
-    PLAN_CUENTAS_CRUD_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_crud/v1',
-    PLAN_CUENTAS_MID_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_mid/v1',
-    PLAN_CUENTAS_MONGO_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_mongo_crud/v1',
+    PLAN_CUENTAS_CRUD_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_crud/v1/',
+    PLAN_CUENTAS_MID_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_mid/v1/',
+    PLAN_CUENTAS_MONGO_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_mongo_crud/v1/',
     CONFIGURACION_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
     NOTIFICACION_SERVICE: 'wss://autenticacion.udistrital.edu.co/apioas/notificacion_ws/v1/',
     CONF_MENU_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
@@ -20,7 +42,7 @@ export const Config = {
       REDIRECT_URL: 'http://10.20.0.254/presupuesto_cliente/',
       SIGN_OUT_URL: 'https://autenticacion.udistrital.edu.co/oidc/logout',
       SIGN_OUT_REDIRECT_URL: 'http://10.20.0.254/presupuesto_cliente/',
-      REFRESH_TOKEN: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/token',
+      REFRESH_TOKEN: 'https://autenticacion.udistrital.edu.co/oauth2/token',
     },
   },
   PREPROD: {
