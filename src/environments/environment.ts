9ddf1f34278508e65ca6edb4768b2b1233073f35
David Morales <daamoralesm@correo.udistrital.edu.co>
2019-07-08 18:31:51 -0500

    #35 #36 add build enviroment
    se crean los diferentes build para cada ambiente, se corrigen importaciones y se agrega libreria de autenticacion oidc-auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,25 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
   production: false,
-  entorno: 'LOCAL',
-  // entorno: 'PREPROD',
+  NUXEO: {
+    PATH: 'https://documental.udistrital.edu.co/nuxeo/',
+  },
+  PLAN_CUENTAS_CRUD_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_crud/v1/',
+  PLAN_CUENTAS_MID_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_mid/v1/',
+  PLAN_CUENTAS_MONGO_SERVICE:'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_mongo_crud/v1/',
+  CONFIGURACION_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
+  NOTIFICACION_SERVICE: 'wss://autenticacion.udistrital.edu.co/apioas/notificacion_ws/v1/',
+  CONF_MENU_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
+  TOKEN: {
+    AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
+    CLIENTE_ID: 'TJ43YHyy3T29GrlTx9l0j7J7Cl8a',
+    RESPONSE_TYPE: 'id_token token',
+    SCOPE: 'openid email role',
+    REDIRECT_URL: 'http://10.20.0.254/presupuesto_cliente/',
+    SIGN_OUT_URL: 'https://autenticacion.udistrital.edu.co/oidc/logout',
+    SIGN_OUT_REDIRECT_URL: 'http://10.20.0.254/presupuesto_cliente/',
+  },
+
 };
