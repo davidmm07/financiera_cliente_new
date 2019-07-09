7ad8cc45711e70c43fbc53ad63d14e9e8e5deaec
macaguegi <mcguerrerog@correo.udistrital.edu.co>
2019-07-09 16:11:32 -0500

    se agrega helper de apropiaciones y modelo #33

--- src/app/pages/plan-cuentas/apropiaciones/apropiaciones.component.ts
+++ src/app/pages/plan-cuentas/apropiaciones/apropiaciones.component.ts
@@ -1,38 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { Rubro } from '../../../@core/data/models/rubro';
+import { Component, OnInit } from "@angular/core";
+import { Rubro } from "../../../@core/data/models/rubro";
+import { ApropiacionHelper } from "../../../helpers/apropiaciones/apropiacionHelper";
+import { PopUpManager } from "../../../managers/popUpManager";
 
 @Component({
-  selector: 'apropiaciones',
-  templateUrl: './apropiaciones.component.html',
-  styleUrls: ['./apropiaciones.component.scss']
+  selector: "apropiaciones",
+  templateUrl: "./apropiaciones.component.html",
+  styleUrls: ["./apropiaciones.component.scss"]
 })
 export class ApropiacionesComponent implements OnInit {
-  rubroSeleccionado : any;
-  apropiacionAprobada : boolean;
-  validado=false;
-  constructor() {
+  rubroSeleccionado: any;
+  apropiacionAprobada: boolean;
+  validado = false;
+  constructor(
+    private apHelper: ApropiacionHelper,
+    private popManager: PopUpManager
+  ) {
     this.rubroSeleccionado = {
-      Codigo: '',
-      Nombre: '',
-      };
-   }
-
-  ngOnInit() {
+      Codigo: "",
+      Nombre: ""
+    };
   }
+
+  ngOnInit() {}
 
   receiveMessage($event) {
-    console.log($event);
     this.rubroSeleccionado = <Rubro>$event
-
-    console.log(this.rubroSeleccionado);
+    this.rubroSeleccionado.Id = parseInt(this.rubroSeleccionado.Id, 0);
+    this.rubroSeleccionado.UnidadEjecutora = parseInt(this.rubroSeleccionado.UnidadEjecutora, 0);
   }
 
-  aprobarApropiacion(){
+  aprobarApropiacion() {
     this.apropiacionAprobada = true;
   }
 
-
-  onClose(){
+  onClose() {
     this.apropiacionAprobada = false;
   }
+
+  registrarApropiacionARubro(event) {
+    if (event.valid) {
+      event.data.RubroSel = typeof this.rubroSeleccionado.Codigo === 'undefined' ? undefined : this.rubroSeleccionado;
+      event.data.RubroHijo.Codigo = typeof this.rubroSeleccionado.Codigo === 'undefined' ?
+        event.data.RubroHijo.Codigo + '' :
+
+
+    this.apHelper.apropiacionRegister(event.data).subscribe(res => {
+      if (res) {
+        this.popManager.showSuccessAlert("Se registro el Rubro correctamente!");
+        //this.cleanForm()
+        //this.eventChange.emit(true);
+      } else {
+        this.popManager.showErrorAlert("Datos Erroneos");
+      }
+    });
+  }
+}
 }
