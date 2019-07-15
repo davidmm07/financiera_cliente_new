1f24d12863967fa18b712ae2d7ecd92ecbeb46cb
macaguegi <mcguerrerog@correo.udistrital.edu.co>
2019-07-15 13:03:33 -0500

    ajustes modulo de apropiaciones

--- src/app/pages/plan-cuentas/apropiaciones/apropiaciones.component.ts
+++ src/app/pages/plan-cuentas/apropiaciones/apropiaciones.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Rubro } from "../../../@core/data/models/rubro";
 import { ApropiacionHelper } from "../../../helpers/apropiaciones/apropiacionHelper";
 import { PopUpManager } from "../../../managers/popUpManager";
+import { Apropiacion } from '../../../@core/data/models/apropiacion';
 
 @Component({
   selector: "apropiaciones",
@@ -10,10 +11,13 @@ import { PopUpManager } from "../../../managers/popUpManager";
 })
 export class ApropiacionesComponent implements OnInit {
   rubroSeleccionado: any;
+  arbolRubro : any;
+  info_apropiacion: Apropiacion; 
   apropiacionAprobada: boolean;
   vigenciaSeleccionada: any;
   valorApropiacion: any;
   validado = false;
+  clean = false;
   constructor(
     private apHelper: ApropiacionHelper,
     private popManager: PopUpManager
@@ -26,7 +30,9 @@ export class ApropiacionesComponent implements OnInit {
     this.vigenciaSeleccionada = 0;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.info_apropiacion = {} as Apropiacion;
+  }
 
   receiveMessage($event) {
     this.rubroSeleccionado = <Rubro>$event;
@@ -38,8 +44,8 @@ export class ApropiacionesComponent implements OnInit {
   }
 
   receiveApropiationValue($event) {
-    this.valorApropiacion = "hhh";
-    console.log("Camila la mejor" + this.valorApropiacion)
+    this.valorApropiacion = "";
+    console.log(this.valorApropiacion)
   }
 
   aprobarApropiacion() {
@@ -71,6 +77,37 @@ export class ApropiacionesComponent implements OnInit {
           this.popManager.showErrorAlert("Datos Erroneos");
         }
       });
+    }
+  }
+
+
+  cleanForm() {
+  /*   this.clean = !this.clean;
+    this.rubroSeleccionado = {};
+    this.info_rubro = null;
+    this.formInfoRubro.campos[FormManager.getIndexForm(this.formInfoRubro, 'Codigo')].prefix.value = ''; */
+
+  }
+
+
+
+  validarForm(event) {
+    if (event.valid) {
+      event.data.RubroPadre = typeof this.rubroSeleccionado.Codigo === 'undefined' ? undefined : this.rubroSeleccionado;
+
+      event.data.RubroHijo.Codigo = typeof this.rubroSeleccionado.Codigo === 'undefined' ?
+        event.data.RubroHijo.Codigo + '' :
+        this.rubroSeleccionado.Codigo + '-' + event.data.RubroHijo.Codigo;
+
+      this.apHelper.apropiacionRegister(event.data).subscribe((res) => {
+        if (res) {
+          this.popManager.showSuccessAlert('Se registro la preasignación de apropiación correctamente!');
+          this.cleanForm()
+          //this.eventChange.emit(true);
+        }
+      });
+    } else {
+      this.popManager.showErrorAlert('Datos Incompletos');
     }
   }
 }
