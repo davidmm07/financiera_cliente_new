08e86d3b1d9699e6076990d591589ca93b9bba22
juancsr <jcsarmientor@correo.udistrital.edu.co>
2019-07-21 14:48:23 -0500

    Cliente para asociación de fuentes: * Se vinculan los rubros a las dependencias * Se deja el objeto con los rubros asignados a las dependencias issue #27

--- src/app/pages/plan-cuentas/fuentes/fuentes.component.ts
+++ src/app/pages/plan-cuentas/fuentes/fuentes.component.ts
@@ -54,7 +54,7 @@ export class FuentesComponent implements OnInit {
   rubrosAsignados: any = [];
   dependencias: any = [];
   dependenciasAsociadas: any = {}
-  dependencia: any;
+  dependenciaSeleccionada: any;
 
   formDependenciarubro = FORM_DEPENDENCIA_RUBRO;
 
@@ -74,22 +74,34 @@ export class FuentesComponent implements OnInit {
     console.info("event", event);
   }
 
-  asignarDependencia($event, rubro: Rubro) {
-    console.log(this.dependencia);
+  asignarDependencia($event: any, rubro: Rubro) {
+    console.info()
+    this.verificarAsignacionDependencia(rubro, this.dependencias[$event]);
+  }
+  
+  verificarAsignacionDependencia(rubro: Rubro, dependenciaAsignada: any) {
     this.rubrosAsignados.filter((data) => {
-      data['dependencias'][this.dependencia] = 0;
+      if (data === rubro) {
+        console.log(data['Dependencias'])
+        console.log(data['Dependencias'][0])
+        for (let i = 0; i < data['Dependencias'].length; i++) {
+          if (data['Dependencias'][i] === -1) {
+            data['Dependencias'][i] = dependenciaAsignada;
+          }
+        }
+      }
     });
   }
 
   agregarDependencia($event, rubro: Rubro) {
     this.rubrosAsignados.filter((data) => {
-      data['dependencias'].push([this.dependencias[0]]);
+    (data === rubro ); data['Dependencias'].push(-1)
     });
   }
 
   receiveMessage($event) {
     if (this.rubrosAsignados.filter((data) => (data.Codigo === $event.Codigo)).length === 0) {
-      $event['dependencias'] = [this.dependencias[0]];
+      $event['Dependencias'] = [-1];
       this.rubrosAsignados = [...this.rubrosAsignados, $event];
     }
   }
